e4085f69bd2db4928e8213cc88d6d92685d231d4
shurinm <shurin-m@mail.ru>
2016-06-16 11:27:30 +0600

    Update file manager

--- Fail_Manager.c
+++ Fail_Manager.c
@@ -4,81 +4,177 @@
 #include<termios.h>
 #include<sys/ioctl.h>
 #include<sys/types.h>
+#include <sys/stat.h>
 #include<dirent.h>
 #include<stdio.h>   
 #include <string.h>
 #include<unistd.h>
+#include <string.h>
 
-WINDOW *create_box (int sy, int sx, int py, int px)                             //VIVOD OKNA
-{
-	WINDOW *nwin;
-	nwin = newwin (sy, sx, py, px);
-	refresh();
-	box(nwin, 0, 0);
-	wrefresh(nwin);
-	return nwin;           
-}
-WINDOW *create_win (int sy, int sx, int py, int px)
-{
-	WINDOW *nwin;
-	nwin = newwin (sy, sx, py, px);
-	wrefresh(nwin);
-	return nwin;
-}	
-	
+#define LEFT    0
+#define RIGHT   1
+#define BLEFT   2
+#define BRIGHT  3
 
-int get_dir (char *dname, WINDOW *mywin, char **data)                            //vivod dirrectorii
+int get_content (char *dname, char content[512][128])                            //vivod dirrectorii
 {
-	int i;
 	DIR *dir;
-	struct dirent *entry;
+	struct dirent *dinfo;
+	unsigned count = 0;
+	
+	//Открываем директорию
 	dir = opendir (dname);
-
 	if (!dir)
-	{
-		endwin();
-		perror ("diropen");
-        	exit(1);
-	}
+		return -1;
 
-	for (i = 0; (entry = readdir(dir)) !=NULL; i++)
-	{
-		data[i] = malloc(sizeof(char) * 512);
-		strcpy(data[i], entry->d_name);
-	}
+	//Считываем содержимое директории
+	for (; (dinfo = readdir(dir)) !=NULL; count++)
+		strcpy(content[count], dinfo->d_name);
+	content[count][0] = '\0';
 
 	closedir(dir);
-	return i;
+	return count;
 } 
 
-void ref_data(char **data, unsigned count, unsigned select, WINDOW *win)
+//Функция обновляет все окна
+void refresh_manager(WINDOW **win, char content[2][512][128], unsigned active, unsigned print, unsigned select)
 {
-	for(int i = 0; i< count; i++)
-		mvwprintw(win, i, 0, data[i]);
-	if(select != -1)
-	{
-		wattron(win, COLOR_PAIR(1));
-		mvwprintw(win, select, 0, data[select]);
-		wattroff(win, COLOR_PAIR(1));
-	}
-	wrefresh(win);
-}	
-        
-int main (int argc, char ** argv)
+	unsigned p;
 
-{
-	WINDOW * win_l;
-	WINDOW * win_bl;                            
-	WINDOW * win_r;
-	WINDOW * win_br;
-	WINDOW *win_active;
-	char **data_r;
-	char **data_l;
-	char **data_current;
-	unsigned count_l = 0;
-	unsigned count_r = 0;
-	unsigned count_active = 0;
+	//Очистка экрана
+	for(int i = 0; i < 4; i++)
+		wclear(win[i]);
 	
+	//Вывод рамок
+	box(win[BLEFT], 0, 0);
+	box(win[BRIGHT],0 ,0);
+	//refresh();
+
+	//Вывод содержимого директорий
+	for(int i = 0; i < 2; i++)
+	{	
+		if(i == active)
+			p = print;
+		else p = 0;
+		for(int j = 0; content[i][p][0] != '\0'; p++, j++)
+			mvwprintw(win[i], j, 3, content[i][p]);
+	}
+	mvwprintw(win[active], select, 0, " > ");
+
+	//Обновление окон
+	for(int i = 3; i >= 0; i--)
+		wrefresh(win[i]);
+}
+
+//Функция инициализирует графику и все переменные
+void init_manager(WINDOW ***win, char content[2][512][128], unsigned count[2], char patch[2][512], unsigned *active, unsigned *print, unsigned *select)
+{
+	//Инициализация графики
+	initscr();
+	cbreak();
+	curs_set(0);
+	noecho();
+	keypad(stdscr, TRUE);
+	
+	//Выделение памяти
+	(*win) = malloc(4 * sizeof(WINDOW *));
+	
+	//Инициализации переменных
+	(*active) = LEFT;
+	(*print) = 0;
+	(*select) = 0;
+	getcwd(patch[LEFT], 512);
+	getcwd(patch[RIGHT], 512);
+	count[LEFT] = get_content(patch[LEFT], content[LEFT]);
+	count[RIGHT] = get_content(patch[RIGHT], content[RIGHT]);
+	(*win)[BLEFT] = newwin(LINES, COLS / 2, 0, 0);
+	(*win)[BRIGHT] = newwin(LINES, COLS / 2, 0, COLS / 2);
+	(*win)[LEFT] = newwin(LINES - 2, (COLS / 2) - 2, 1, 1);
+	(*win)[RIGHT] = newwin(LINES - 2, (COLS / 2) - 2, 1, (COLS / 2) + 1);
+	
+	refresh();
+}
+
+void open_dir(char* name, char patch[2][512], char content[512][128], unsigned *count, unsigned active)
+{
+	struct stat info;
+	
+	stat(name, &info);
+	if(info.st_mode == 16877)
+	{
+		chdir(name);
+		getcwd(patch[active], 512);
+		(*count) = get_content(patch[active], content);
+	}
+}
+
+//Функция отключает графику
+void destroy_manager()
+{
+	endwin();
+}
+
+int main ()
+{
+	WINDOW **win;			//Окна
+	char content[2][512][128];	//Содержимое окон
+	char patch[2][512];		//Директории в которых мы находимся
+	unsigned count[2];
+	unsigned active;		//Индекс активного окна
+	unsigned print;
+	unsigned select;
+	int key = 0;
+
+	init_manager(&win, content, count, patch, &active, &print, &select);
+	refresh_manager(win, content, active, print, select);
+		
+	while((key = getch()) != 27)
+	{
+		switch(key)
+		{
+			case KEY_DOWN:
+				if(print + select >= count[active] - 1)
+					continue;
+				if(select >= LINES - 3)
+					print++;
+				else
+					select++;
+			break;
+			case KEY_UP:
+				if(print <= 0 && select <= 0)
+					continue;
+				if(select <= 0)
+					print--;
+				else
+					select--;
+			break;
+			case KEY_LEFT:
+				if(active == LEFT)
+					continue;
+				active = LEFT;
+				print = 0;
+				select = 0;
+				chdir(patch[active]);
+			break;
+			case KEY_RIGHT:
+				if(active == RIGHT)
+					continue;
+				active = RIGHT;
+				print = 0;
+				select = 0;
+				chdir(patch[active]);
+			break;
+			case '\n':
+				open_dir(content[active][select + print], patch, content[active], &(count[active]), active);
+				print = 0;
+				select = 0;
+			break;
+		}
+		refresh_manager(win, content, active, print, select);
+	}
+	destroy_manager();
+}
+/*	
 	data_current = data_l = malloc(sizeof(char*) * 512);
 	data_r = malloc(sizeof(char*) * 512);
 	
@@ -139,14 +235,17 @@ int main (int argc, char ** argv)
 			case 	'\n':
 				if(win_active == win_l)
 				{
-					chdir(data_l[cline]);
+					//chdir(data_l[cline]);
 					count_l = get_dir(data_l[cline], win_l, data_l);
 					count_active = count_l;
 					data_current = data_l;
 					
 				}				
 			break;
-
+			case KEY_F(4):
+				execl("/home/2016/shurin/File_Manager/texteditor", "texteditor", (char*)0);
+				_exit(0);
+			break;
 		}
 	}
 				
@@ -154,5 +253,5 @@ int main (int argc, char ** argv)
 	getch ();
 	endwin ();
 	exit (EXIT_SUCCESS);
-}
+*/
 
